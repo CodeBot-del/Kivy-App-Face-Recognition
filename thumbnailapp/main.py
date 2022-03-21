ce04fe84db11fb5777389c422fcac19ff06da894
gracie2 <stevenmanangu360@gmail.com>
2022-03-21 11:50:23 +0300

    removed background from image successfully

--- thumbnailapp/main.py
+++ thumbnailapp/main.py
@@ -9,12 +9,12 @@ import numpy as np
 class MainApp(MDApp):
     def build(self):
         layout = MDBoxLayout(orientation='vertical')
-        self.image = Image(source="cena.jpeg")
+        self.image = Image(source="sphere.jpg")
         self.label = MDLabel()
         layout.add_widget(self.image)
         layout.add_widget(self.label)
         self.save_img_button = MDRaisedButton(
-            text="CLICK HERE",
+            text="Remove BG",
             pos_hint={'center_x': .5, 'center_y': .5},
             size_hint=(None, None)
         )
@@ -31,14 +31,16 @@ class MainApp(MDApp):
         foregroundModel = np.zeros((1,65), np.float64)
         
         #mark the image    
-        rectangle = (0,0, int(self.image.texture_size[1], int(self.image.texture_size[0])))
+        rectangle = (0,0, int(self.image.texture_size[1]), int(self.image.texture_size[0]))
+        # rectangle = (0,0, 16, 16)
         
-        values = (
-            ("Definite Background", cv2.GC_BGD),
-            ("Probable Background", cv2.GC_PR_BGD),
-            ("Definite Foreground", cv2.GC_FGD),
-            ("Probable Foreground", cv2.GC_PR_FGD),
-        )
+        #parameters passed to the grabcut method
+        # values = (
+        #     ("Definite Background", cv2.GC_BGD),
+        #     ("Probable Background", cv2.GC_PR_BGD),
+        #     ("Definite Foreground", cv2.GC_FGD),
+        #     ("Probable Foreground", cv2.GC_PR_FGD),
+        # )
         
         #crop the foreground 
         cv2.grabCut(image, mask, rectangle, backgroundModel, foregroundModel, 4, cv2.GC_INIT_WITH_RECT)
@@ -53,6 +55,11 @@ class MainApp(MDApp):
         b, g, r = cv2.split(image)
         gray_layer = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         _, alpha = cv2.threshold(gray_layer, 0, 255, cv2.THRESH_BINARY)
+        
+        rgba = [b, g, r, alpha]
+        destination = cv2.merge(rgba, 4)
+        cv2.imwrite("results2.png", destination)
+        self.label.text = "Removed Background successfully"
 
     
     
